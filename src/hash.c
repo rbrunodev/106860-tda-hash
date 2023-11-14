34321f363c00bba962da1affb71d97fb601560c2
rbruno22 <rbruno@libgot.com>
2023-11-14 16:49:41 -0300

    test indice hash

--- src/hash.c
+++ src/hash.c
@@ -36,12 +36,79 @@ hash_t *hash_crear(size_t capacidad)
 	return hash;
 }
 
+unsigned long indice_hash(const char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}
+
+void hash_redimensionar(hash_t *hash, size_t nueva_capacidad)
+{
+	if (!hash)
+		return;
+
+	nodo_t **tabla_nueva = calloc(nueva_capacidad, sizeof(nodo_t*));
+	if (!tabla_nueva)
+		return;
+
+	for (size_t i = 0; i < hash->capacidad; i++) {
+		nodo_t *nodo = hash->tabla[i];
+		while (nodo) {
+			nodo_t *nodo_a_mover = nodo;
+			nodo = nodo->siguiente;
+			size_t indice = indice_hash(nodo_a_mover->clave) % nueva_capacidad;
+			nodo_a_mover->siguiente = tabla_nueva[indice];
+			tabla_nueva[indice] = nodo_a_mover;
+		}
+	}
+
+	free(hash->tabla);
+	hash->tabla = tabla_nueva;
+	hash->capacidad = nueva_capacidad;
+}
+
+bool agregar_capacidad(hash_t *hash){
+	if(!hash)
+		return false;
+	
+	size_t factor_carga = hash->cantidad / hash->capacidad;
+
+	return factor_carga >= FACTOR_CARGA_MAXIMO;
+}
+
+/*
+ * Inserta o actualiza un elemento en el hash asociado a la clave dada.
+ *
+ * Si la clave ya existía y se reemplaza el elemento, se almacena un puntero al
+ * elemento reemplazado en *anterior, si anterior no es NULL.
+ *
+ * Si la clave no existía y anterior no es NULL, se almacena NULL en *anterior.
+ *
+ * La función almacena una copia de la clave provista por el usuario,
+ *
+ * Nota para los alumnos: Recordar que si insertar un elemento provoca
+ * que el factor de carga exceda cierto umbral, SE DEBE AJUSTAR EL
+ * TAMAÑO DE LA TABLA PARA EVITAR FUTURAS COLISIONES.
+ *
+ * Devuelve el hash si pudo guardar el elemento o NULL si no pudo.
+ */
 hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
 		      void **anterior)
 {
 	if (!hash || !clave)
 		return NULL;
 
+	if (agregar_capacidad(hash)){
+		// if(!hash_redimensionar(hash, hash->capacidad * 2))
+		// 	return NULL;
+	}
+	size_t indice = indice_hash(clave) % hash->capacidad;
+
+	print("indice: %lu\n", indice);
 	return NULL;
 }
 
