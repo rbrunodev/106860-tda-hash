4168d08c2a52e65bfa0b7297f29370911b4df677
rbruno22 <rbruno@libgot.com>
2023-11-17 22:29:49 -0300

    Add pruebas crear hash

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,36 @@
 #include "pa2m.h"
+#include "hash.h"
 
-void prueba_simple()
+void prueba_crear()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	size_t capac = 3;
+	hash_t *hash = hash_crear(capac);
+	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 3");
+	hash_destruir(hash);
+
+	hash = hash_crear(0);
+	pa2m_afirmar(
+		hash != NULL,
+		"Prueba crear un hash con capacidad 0 y lo crea con capacidad 3");
+	hash_destruir(hash);
+
+	hash = hash_crear(10);
+	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 10");
+	hash_destruir(hash);
+
+	hash = hash_crear(100);
+	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 100");
+	hash_destruir(hash);
 }
 
 int main()
 {
 	pa2m_nuevo_grupo(
 		"\n======================== XXX ========================");
-	prueba_simple();
+
+	pa2m_nuevo_grupo(
+		"\n=============== PRUEBAS DE CREACION Y DESTRUCCION ===========");
+	prueba_crear();
 
 	return pa2m_mostrar_reporte();
 }
