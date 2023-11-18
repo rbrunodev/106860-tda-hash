967edd75867b2c24b005c5508f61b7b2cd208d42
rbruno22 <rbruno@libgot.com>
2023-11-17 22:38:15 -0300

    prueba_insertar

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -8,18 +8,43 @@ void prueba_crear()
 	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 3");
 	hash_destruir(hash);
 
-	hash = hash_crear(0);
-	pa2m_afirmar(
-		hash != NULL,
-		"Prueba crear un hash con capacidad 0 y lo crea con capacidad 3");
-	hash_destruir(hash);
-
 	hash = hash_crear(10);
 	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 10");
 	hash_destruir(hash);
 
 	hash = hash_crear(100);
 	pa2m_afirmar(hash != NULL, "Prueba crear un hash con capacidad 100");
+	hash_destruir(hash);
+
+	hash = hash_crear(2);
+	pa2m_afirmar(
+		hash != NULL,
+		"Prueba crear un hash con capacidad 2 y lo crea con capacidad 3");
+	hash_destruir(hash);
+}
+
+void prueba_insertar()
+{
+	hash_t *hash = hash_crear(5);
+	char *clave1 = "clave1";
+	// char *clave2 = "clave2";
+	// char *clave3 = "clave3";
+	// char *clave4 = "clave4";
+	// char *clave5 = "clave5";
+	// char *clave6 = "clave6";
+	// char *clave7 = "clave7";
+
+	char *valor1 = "valor1";
+	// char *valor2 = "valor2";
+	// char *valor3 = "valor3";
+	// char *valor4 = "valor4";
+	// char *valor5 = "valor5";
+	// char *valor6 = "valor6";
+	// char *valor7 = "valor7";
+
+	pa2m_afirmar(hash_insertar(hash, clave1, valor1, NULL) == hash &&
+			     hash_cantidad(hash) == 1,
+		     "Se inserta clave1 con valor1");
 	hash_destruir(hash);
 }
 
@@ -31,6 +56,8 @@ int main()
 	pa2m_nuevo_grupo(
 		"\n=============== PRUEBAS DE CREACION Y DESTRUCCION ===========");
 	prueba_crear();
+
+	pa2m_nuevo_grupo("\n=============== PRUEBAS DE INSERTAR ===========");
 
 	return pa2m_mostrar_reporte();
 }
