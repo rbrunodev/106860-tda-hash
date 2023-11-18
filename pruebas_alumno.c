873e4bd1082d03a41ef0a4831be4ecd9fc50ab4c
rbruno22 <rbruno@libgot.com>
2023-11-17 23:23:01 -0300

    dd

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,5 +1,6 @@
 #include "pa2m.h"
 #include "src/hash.h"
+#include <stdio.h>
 
 void prueba_crear()
 {
@@ -166,6 +167,7 @@ void prueba_iterador()
 
 	iteraciones =
 		hash_con_cada_clave(hash, continuar_iterando, (void *)clave4);
+	printf("iteraciones: %zu\n", iteraciones);
 	pa2m_afirmar(
 		iteraciones == 3,
 		"La iteración devuelve la cantidad correcta después de recorrer 3 claves");
