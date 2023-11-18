319cc3999f4c64522e74829e3e61a1b57befed2d
rbruno22 <rbruno@libgot.com>
2023-11-17 23:06:37 -0300

    Pruebas de cantidad

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -42,42 +42,49 @@ void prueba_insertar()
 		hash_insertar(hash, clave1, valor1, NULL) == hash &&
 			hash_cantidad(hash) == 1,
 		"Se inserta clave1 con valor1 y se obtiene la clave1 con valor1");
+	pa2m_afirmar(hash_cantidad(hash) == 1, "La cantidad es 1");
 
 	pa2m_afirmar(
 		hash_insertar(hash, clave2, valor2, NULL) == hash &&
 			hash_cantidad(hash) == 2,
 		"Se inserta clave2 con valor2 y se obtiene la clave2 con valor2");
+	pa2m_afirmar(hash_cantidad(hash) == 2, "La cantidad es 2");
 
 	pa2m_afirmar(
 		hash_insertar(hash, clave3, valor3, NULL) == hash &&
 			hash_cantidad(hash) == 3 &&
 			hash_obtener(hash, clave3) == valor3,
 		"Se inserta clave3 con valor3 y se obtiene la clave3 con valor3");
-
+	pa2m_afirmar(hash_cantidad(hash) == 3, "La cantidad es 3");
+	
 	pa2m_afirmar(
 		hash_insertar(hash, clave3, valor4, NULL) == hash &&
 			hash_cantidad(hash) == 3 &&
 			hash_obtener(hash, clave3) == valor4,
 		"Se inserta clave3 con valor4 y se obtiene la clave3 con valor4");
-
+	pa2m_afirmar(hash_cantidad(hash) == 3, "La cantidad sigue siendo 3, se actualiza el valor de la clave");
+	
 	pa2m_afirmar(
 		hash_insertar(hash, clave4, valor4, NULL) == hash &&
 			hash_cantidad(hash) == 4 &&
 			hash_obtener(hash, clave4) == valor4,
 		"Se inserta clave4 con valor4 y se obtiene la clave4 con valor4");
+	pa2m_afirmar(hash_cantidad(hash) == 4, "La cantidad es 4");
 
 	pa2m_afirmar(
 		hash_insertar(hash, clave5, valor5, NULL) == hash &&
 			hash_cantidad(hash) == 5 &&
 			hash_obtener(hash, clave5) == valor5,
 		"Se inserta clave5 con valor5 y se obtiene la clave5 con valor5");
+	pa2m_afirmar(hash_cantidad(hash) == 5, "La cantidad es 5");
 
 	pa2m_afirmar(
 		hash_insertar(hash, clave5, valor1, NULL) == hash &&
 			hash_cantidad(hash) == 5 &&
 			hash_obtener(hash, clave5) == valor1,
 		"Se inserta clave5 con valor1 y se obtiene la clave5 con valor1");
-
+	pa2m_afirmar(hash_cantidad(hash) == 5, "La cantidad sigue siendo 5, se actualiza el valor de la clave");
+	
 	hash_destruir(hash);
 }
 
